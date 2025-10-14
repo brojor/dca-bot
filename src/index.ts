66773d0a49d0df6a2936d0434e1f70cf830fc53c
brojor <bronislav.jordan@gmail.com>
2025-10-14 09:59:59 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { CoinbaseAdvTradeClient, CoinbaseAdvTradeCredentials, AccountsService } from 'coinbase-advanced-sdk/dist';
+import '@dotenvx/dotenvx/config'
+
+const KEY_NAME = process.env.KEY_NAME;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const credentials = new CoinbaseAdvTradeCredentials(
+  KEY_NAME,
+  PRIVATE_KEY
+);
+
+const client = new CoinbaseAdvTradeClient(credentials);
+
+const accountService = new AccountsService(client);
+
+
+accountService
+  .listAccounts({})
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
